c77b7f690cbf541e9dcf99a5c3050db27f090c86
Thiru <jsthir@gmail.com>
2016-12-19 01:06:09 +0530

    changed the package name

--- src/main/java/com/hr/register/RegistrationApplication.java
+++ src/main/java/com/hr/register/RegistrationApplication.java
@@ -1,4 +1,4 @@
-package com.test.register;
+package com.hr.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hr/register/RegistrationApplicationTests.java
+++ src/test/java/com/hr/register/RegistrationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.test.register;
+package com.hr.register;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
